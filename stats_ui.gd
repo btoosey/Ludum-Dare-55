3580acf55c9e223c3c62126367a491273de3e761
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-04-13 22:51:12 +0100

    ADD: Basic art/fonts
    Changed game resolution and updated art/fonts.

--- stats_ui.gd
+++ stats_ui.gd
@@ -1,9 +1,9 @@
 class_name StatsUI
 extends HBoxContainer
 
-@onready var health: HBoxContainer = $Health
+@onready var health: VBoxContainer = $Health
 @onready var health_label: Label = %HealthLabel
-@onready var attack: HBoxContainer = $Attack
+@onready var attack: VBoxContainer = $Attack
 @onready var attack_label: Label = %AttackLabel
 
 func update_stats(stats) -> void:
